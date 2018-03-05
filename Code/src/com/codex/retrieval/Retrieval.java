c328c2468a1ec02931244da639035208dcbd6663
starpxy <7301121@qq.com>
2018-03-05 20:11:57 +0800

    update workspace and some pages

--- Code/src/com/codex/retrieval/Retrieval.java
+++ Code/src/com/codex/retrieval/Retrieval.java
@@ -1,5 +1,12 @@
 package com.codex.retrieval;
-
+/**
+ * 
+ * @author Star
+ * 
+ * Interface for Retrieval part.
+ * 
+ *
+ */
 public interface Retrieval {
-
+	public String Search(String pref,String query);
 }
