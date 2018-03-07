f4ff6be0dac83edfabfb49e977ade1b00a74a4e3
CiaranC123 <ciaran.connolly.1@ucdconnect.ie>
2018-03-07 15:33:59 +0800

    Windows Server Connection Test File

---
+++ Ciaran's Workspace/Cleanup/TestWindowsServer.py
@@ -0,0 +1,20 @@
+# coding=utf-8
+"""
+Created on 06/03/2018
+Author: Ciarán
+"""
+
+import paramiko
+
+def main():
+    try:
+        ssh_client = paramiko.SSHClient()
+        ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh_client.connect(hostname="118.89.233.227", username="Administrator", password="Star==960906")
+        print("Connection successful")
+    except Exception as e:
+        print("Could not connect: " + str(e))
+
+
+if __name__ == "__main__":
+    main()
