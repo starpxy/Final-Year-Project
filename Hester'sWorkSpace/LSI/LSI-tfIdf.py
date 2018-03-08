c245ead6b646496a86e48b14e9ba11a80c0028a0
hesterzmy <584363258@qq.com>
2018-03-08 13:01:11 +0800

    search on the Json files got from Ciaran

--- Hester'sWorkSpace/LSI/LSI-tfIdf.py
+++ Hester'sWorkSpace/LSI/LSI-tfIdf.py
@@ -1,4 +1,3 @@
-from numpy import zeros
 import numpy as np
 from scipy.linalg import *
 from scipy import spatial
@@ -12,65 +11,46 @@ path = "../files" #path name
 files= os.listdir(path) #get all the file names
 documents = {}
 contents=[]
+wholeContent=""
 i=0
-for file in files: #遍历文件夹
-     if not os.path.isdir(file): #判断是否是文件夹，不是文件夹才打开
+for file in files: #go through the folder
+     if not os.path.isdir(file): #judge if it is a folder
         documents[i]=conv.to_dic(path+"/"+file)
         contents.append(documents[i]['content'])
+        wholeContent+=documents[i]['content']
         i+=1
 
-stopwords = ['and', 'edition', 'for', 'in', 'little', 'of', 'the', 'to']
+stopwords = ['and', 'edition', 'for', 'in', 'little', 'of', 'the', 'to','print']
 ignorechars =":!,"
 vectorizer = CountVectorizer()
 tfidf = TfidfVectorizer()
-X = vectorizer.fit_transform(contents)
+X = vectorizer.fit_transform([wholeContent]).toarray().T
 transformer=TfidfTransformer()
 re = tfidf.fit_transform(contents).toarray().T #tf-idf values
 word=vectorizer.get_feature_names()#the unique terms after preprocessing
-dic={}#dictionary{term: tf-idf}
-tranIgnore = str.maketrans("", "", ignorechars)#ignore the ignorechars
-for i in range(len(word)):
-    w=word[i]
-    if w not in stopwords:
-        dic[w]=re[i]
-print('dic\n')
-print(dic)
-dickeys=[]#keys of dic
-print(contents)
+print(re.shape)
+i=0
+#throw away the low-frequency terms (terms appears only once)
+while True:
+    if i>=len(word):
+        break
+    if X[i]<2:
+        word.pop(i)
+        X=np.delete(X,i,0)
+        re=np.delete(re,i,0)
+    else:
+        i+=1
+
+print(re.shape)
 
 class LSA(object):
     def __init__(self, stopwords, ignorechars):
         self.stopwords = stopwords
-        self.ignorechars = ignorechars
-        self.wdict = {}
-        self.dcount = 0
-        self.dic1={}
-        self.dickeys=[]
-    def parse(self, doc):
-        words = doc.split()
-        for w in words:
-            w = w.lower().replace("'s", '').translate(tranIgnore)
-            if w in self.stopwords:
-                continue
-            elif w not in word:
-                self.dic1[w]=dic[w]#give the words a tf-idf value if it appears in more than one document(when we detect a term not in the word list, it is the second time to appear)
-            else:
-                word.remove(w) #delete the term from word when this term is found at the first time
-        self.dcount += 1
-
-    def build(self):
-        self.dickeys=sorted(self.dic1.keys())
-        self.A = zeros([len(self.dickeys), len(contents)])#num(terms)*num(documments)
-        #construct the matrix A
-        index=0
-        for i in self.dickeys:
-            self.A[index] = self.dic1[i]
-            index+=1
 
     def printA(self):
-        print('A\n')
-        print (self.A)
-        u,s,vt = svd(self.A,full_matrices=False)
+        print('re\n')
+        print (re)
+        u,s,vt = svd(re,full_matrices=False)
         print ("""\r""")
         print('u\n')
         print (u)
@@ -88,43 +68,54 @@ class LSA(object):
         plt.xlabel(u'dimention2')
         plt.ylabel(u'dimention3')
 
+        ut = u.T  # Transaction
+        demention2 = ut[1]
+        demention3 = ut[2]
+        # draw terms
+        for i in range(len(demention2)):
+            plt.text(demention2[i], demention3[i], word[i])
+            plt.plot(demention2[i], demention3[i], '*')  # draw points
+
         vdemention2 = vt[1]#we choose the 2nd and 3rd dimensions because the first dimension has little meaning
         vdemention3 = vt[2]
+        #get query coordinates
+        qArr=self.getQuery(u,s,d)
+        dX=np.dot(demention2,qArr)
+        dY=np.dot(demention3,qArr)
+        #draw query point
+        plt.text(dX, dY, 'QUERY')
+        plt.plot(dX, dY,  'ro')
+        euclideanSimilarity={}
         for j in range(len(vdemention2)):
             plt.text(vdemention2[j],vdemention3[j],'T'+str(j))
             plt.plot(vdemention2, vdemention3, '.')
+            #calculate the eclidean distance similarity
+            euclideanSimilarity[j]=np.linalg.norm(np.array([vdemention2[j],vdemention3[j]])-np.array([dX,dY]))
 
-        ut = u.T #Transaction
-        demention2 = ut[1]
-        demention3 = ut[2]
-        #draw terms
-        for i in range(len(demention2)):
-            plt.text(demention2[i],demention3[i],self.dickeys[i])
-            plt.plot(demention2[i], demention3[i], '*')#draw points
+        #sort similarity and print out
+        euclidianSimilarity = sorted(euclideanSimilarity.items(), key=lambda item: item[1])
+        for i in range(len(euclidianSimilarity)):
+            index = euclidianSimilarity[i][0]
+            print(str(euclidianSimilarity[i]) + ':    ' + files[index])
 
-        #get query, draw the query point
-        qArr=self.getQuery(u,s,d)
-        # draw the query point
-        dX=np.dot(demention2,qArr)
-        dY=np.dot(demention3,qArr)
-        plt.text(dX, dY, 'QUERY')
-        plt.plot(dX, dY,  'ro')
+
 
     def getQuery(self,u,s,d):
         query=input("Please enter your query: ")
         #work out the Xq.T
-        qArr=np.zeros([1, 11])
-        qWords=query.split()
-        for w in qWords:
-            if w not in self.stopwords:
-                w=w.lower().replace("'s", '').translate(tranIgnore)
-                if w in self.dickeys:
-                    index=self.dickeys.index(w)
-                    qArr[0][index]+=1
+        #get the term frequency
+        qFreq = vectorizer.fit_transform([query]).toarray().T#make the vectorizer fit the query
+        qWord=vectorizer.get_feature_names()#the unique terms after preprocessing
+        qArr=np.zeros([1, len(word)])
+        #fill in the term frequency into the empty Xq matrix
+        for w in qWord:
+            i=qWord.index(w)
+            if w in word:
+                j=word.index(w)
+                qArr[0][j]=qFreq[i]
+
         print(qArr)
         sDiagno=np.diag(np.array(s))
-        print("sDiago")
-        print(sDiagno)
         sInv=np.linalg.inv(sDiagno)
         print("sDiago inverse")
         print(sInv)
@@ -134,18 +125,16 @@ class LSA(object):
         Dq=np.dot(qArr,u)
         Dq=np.dot(Dq,sInv)
         print(Dq)
-        similarity={}
-        for i in range(len(d)):
-            similarity[i]=spatial.distance.cosine(Dq, d[i])
-        similarity=sorted(similarity.items(),key=lambda item:item[1])
-        for i in range(len(similarity)):
-            index=similarity[i][0]
-            print(str(similarity[i])+':    '+contents[index])
+        #similarities from Dq=X.T * T * S-1.
+        # similarity={}
+        # for i in range(len(d)):
+        #     similarity[i]=spatial.distance.cosine(Dq, d[i])
+        # similarity=sorted(similarity.items(),key=lambda item:item[1])
+        # for i in range(len(similarity)):
+        #     index=similarity[i][0]
+        #     print(str(similarity[i])+':    '+files[index])
         return qArr.T
 
 mylsa = LSA(stopwords, ignorechars)
-for t in contents:
-    mylsa.parse(t)
-mylsa.build()
 mylsa.printA()
 plt.show()
