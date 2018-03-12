a71307580c2a6b83b240b55a8c1eb472e01af8f8
CiaranC123 <ciaran.connolly.1@ucdconnect.ie>
2018-03-12 17:08:30 +0800

    Updated FCI

--- Ciaran's Workspace/FCI/FormattedCodeInterface.py
+++ Ciaran's Workspace/FCI/FormattedCodeInterface.py
@@ -9,21 +9,27 @@ Every single object represents one file
 
 '''
 
+
 class FormattedCodeInterface:
 
-    __description = '' # From Kirk
-    __author = '' # From Kirk
-    __update_at = '' # Last commmit
-    __save_time = '' # Date and possibly time
+    __description = ''
+    __author = ''
+    __update_at = ''  # Last commit
+    __save_time = ''  # When I create the json file
     __file_name = ''
-    __project_name=''
-    __save_path = '' # On server
-    __language = '' # Programming language
-    __quality = '' # From Kirk
-    __code = '' # File Contents
-    __comments = ''
+    __project_name = ''
+    __save_path = ''  # On server
+    __language = ''
+    __quality = ''  # For Later
+    __content = ''
+    __code = ''  # Content minus comments
+    __comments = ''  # Content minus code
+    __source = ''
+    __url = ''
+    __wiki = False  # Whether the project has a wiki or not
 
-    def __init__(self,description='',author='',update_at='',save_time='',file_name='',project_name='',save_path='',language='',quality='',code='',comments=''):
+    def __init__(self, description='', author='', update_at='', save_time='', file_name='', project_name='',
+                 save_path='', language='', quality='', content='', code='', comments='', url='', wiki=False):
         self.__description = description
         self.__author = author
         self.__update_at = update_at
@@ -33,8 +39,11 @@ class FormattedCodeInterface:
         self.__save_path = save_path
         self.__language = language
         self.__quality = quality
+        self.__content = content
         self.__code = code
         self.__comments = comments
+        self.__url = url
+        self.__wiki = wiki
 
     def set_description(self,description):
         self.__description = description
@@ -90,6 +99,12 @@ class FormattedCodeInterface:
     def get_quality(self):
         return self.__quality
 
+    def set_content(self, content):
+        self.__content = content
+
+    def get_content(self):
+        return self.__content
+
     def set_code(self,code):
         self.__code = code
 
@@ -102,17 +117,31 @@ class FormattedCodeInterface:
     def get_comments(self):
         return self.__comments
 
+    def set_url(self,url):
+        self.__url = url
+
+    def get_url(self):
+        return self.__url
+
+    def set_wiki(self,wiki):
+        self.__wiki = wiki
+
+    def get_wiki(self):
+        return self.__wiki
+
     def to_dictionary(self):
-        dic = {}
-        dic["description"] = self.__description
-        dic["author"] = self.__author
-        dic["update_at"] = self.__update_at
-        dic["save_time"] = self.__save_time
-        dic["file_name"] = self.__file_name
-        dic["project_name"] = self.__project_name
-        dic["save_path"] = self.__save_path
-        dic["language"] = self.__language
-        dic["quality"] = self.__quality
-        dic["code"] = self.__code
-        dic["comments"] = self.__comments
+        dic = {"description": self.__description,
+               "author": self.__author,
+               "update_at": self.__update_at,
+               "save_time": self.__save_time,
+               "file_name": self.__file_name,
+               "project_name": self.__project_name,
+               "save_path": self.__save_path,
+               "language": self.__language,
+               "quality": self.__quality,
+               "content": self.__content,
+               "code": self.__code,
+               "comments": self.__comments,
+               "url": self.__url,
+               "wiki": self.__wiki}
         return dic
