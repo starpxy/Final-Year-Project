29e98be9441da696a0c916659f287144f7de2ad8
starpxy <7301121@qq.com>
2018-03-12 20:46:50 +0800

    update for django and FCI

--- Star's Python Workspace/FreqCounter.py
+++ Star's Python Workspace/FreqCounter.py
@@ -9,37 +9,57 @@ from LogWriter import LogWriter
 import FCIConverter
 from FormattedCodeInterface import FormattedCodeInterface
 
+
 class FreqCounter:
     files = ''
     fci_list = []
     dic = {}
+    pattern = "qwertyuiopasdfghjklzxcvbnm"
 
     def __init__(self, file_path):
         lw = LogWriter()
         lw.write_info_log("Initialize the Frequency Counter Class")
         if not os.path.exists(file_path):
-            lw.write_error_log("Path "+file_path+" doesn't exist!")
+            lw.write_error_log("Path " + file_path + " doesn't exist!")
             exit()
         else:
             files = os.listdir(file_path)
             self.files = files
             for file in files:
-                if len(file)==37:
+                if len(file) == 37:
                     if not os.path.isdir(file):
                         try:
-                            f = FCIConverter.to_fciObject(file_path+"/"+file)
+                            f = FCIConverter.to_fciObject(file_path + "/" + file)
                             self.fci_list.append(f)
                         except:
                             lw.write_error_log("File type error!")
+
     def get_frequency_list(self):
         for o in self.fci_list:
+            # TODO: This get_content() should be get_code() in later version after 09-03-2018
             content = o.get_content()
-            tokens = str(content).split(' ')
-            print(tokens)
+            tokens = content.split()
+            for token in tokens:
+                processed_token = ''
+                for c in token:
+                    c = c.lower()
+                    if c in self.pattern:
+                        processed_token += c
+                    else:
+                        if processed_token == '':
+                            processed_token = ''
+                        elif processed_token in self.dic:
+                            self.dic[processed_token] += 1
+                        else:
+                            self.dic[processed_token] = 1
+                        processed_token = ''
+        return self.dic
 
 def main():
     fc = FreqCounter("files")
-    fc.get_frequency_list()
+    dic = fc.get_frequency_list()
+    print(dic)
+
 
 if __name__ == '__main__':
     main()
---
+++ Star's Python Workspace/Web App/CodEX/search/views.py
@@ -0,0 +1,39 @@
+# coding:utf-8
+# @author: Star
+# @time: 10-03-2018
+import search.supportings.FCIConverter
+from django.shortcuts import render
+from search.supportings.LogWriter import LogWriter
+from search.supportings.FormattedCodeInterface import FormattedCodeInterface
+
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def search(request):
+    q = request.GET['q']
+    p = int(request.GET['p'])
+    total_p = 20;
+    pages = []
+    p_p = p - 5
+    n_p = p + 5
+    while total_p > 0:
+        pages.append(0)
+        total_p -= 1
+    f = [FormattedCodeInterface(project_name="hello", file_name="this.py",
+                                content="this is my content,haha And Content balabala"),
+         FormattedCodeInterface(project_name="hello", file_name="this.py",
+                                content="this is my content,haha And Content balabala"),
+         FormattedCodeInterface(project_name="hello", file_name="this.py",
+                                content="this is my content,haha And Content balabala"),
+         FormattedCodeInterface(project_name="hello", file_name="this.py",
+                                content="this is my content,haha And Content balabala"),
+         FormattedCodeInterface(project_name="hello", file_name="this.py",
+                                content="this is my content,haha And Content balabala")]
+
+    return render(request, 'search-result.html', {'results': f, 'q': q, 'p': p, 'pages': pages, 'p_p': p_p, 'n_p': n_p,'pre':p-1,'next':p+1})
+
+
+def detail(request):
+    return render(request, 'detail.html')
