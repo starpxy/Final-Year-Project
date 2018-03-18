a8e3853f9add9250c8a0e993dcf78c66a8345836
starpxy <7301121@qq.com>
2018-03-18 22:12:35 +0800

    config of deployment

--- Star's Python Workspace/Web App/CodEX/CodEX/wsgi.py
+++ Star's Python Workspace/Web App/CodEX/CodEX/wsgi.py
@@ -8,8 +8,10 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
+import sys
+
+sys.path.append('/home/ubuntu/.local/lib/python3.5/site-packages')
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "CodEX.settings")
 
