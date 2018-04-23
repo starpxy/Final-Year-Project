a8237dd2eb2c935cae7b0f11fe374291e419a274
starpxy <7301121@qq.com>
2018-04-23 21:56:08 +0800

    ready to online

--- Star's Python Workspace/Web App/CodEX/CodEX/config.py
+++ Star's Python Workspace/Web App/CodEX/CodEX/config.py
@@ -7,11 +7,12 @@
 configs = {
     'db': {},
     'paths': {
-        # 'FCI_path': "/home/ubuntu/test_files/json_files/",
-        # "indexing_path":"/home/ubuntu/CodEX/search/supportings/LSI/CodexIndex.pik",
-        'FCI_path': "/Users/quanyewu/Desktop/files",
-        "LSI_indexing_path":"/Users/quanyewu/Desktop/files/CodexIndex.pik",
-        "AST_indexing_path":"/Users/quanyewu/Desktop/files/CodexASTIndex.pik",
+        'FCI_path': "/home/ubuntu/test_files/json_files/",
+        "LSI_indexing_path":"/home/ubuntu/test_files/CodexIndex.pik",
+        "AST_indexing_path":"/home/ubuntu/test_files/CodexASTIndex.pik",
+        # 'FCI_path': "/Users/quanyewu/Desktop/files",
+        # "LSI_indexing_path":"/Users/quanyewu/Desktop/files/CodexIndex.pik",
+        # "AST_indexing_path":"/Users/quanyewu/Desktop/files/CodexASTIndex.pik",
         'unclean_path': '',
         'cleaned_path': '',
     },
